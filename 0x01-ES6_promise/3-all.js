787cc51170861fccb3d86891c5768740ff102852
agwata9 <agwata9@gmail.com>
2023-04-19 17:55:03 +0000

    correction

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,17 +1,10 @@
-import { uploadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
-  return uploadPhoto()
-    .then((response) => {
-      createUser()
-        .then((res) => {
-          console.log(response.body, res.firstName, res.lastName);
-        })
-        .catch(() => {
-          console.error('Signup system offline');
-        });
-    })
-    .catch(() => {
-      console.error('Signup system offline');
-    });
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(`${user.firstName} ${user.lastName}`);
+  } catch (error) {
+    console.error('Signup system offline');
+  }
 }
