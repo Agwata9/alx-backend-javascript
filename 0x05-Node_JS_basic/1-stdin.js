38922aeecf1a8bf69557d2a9d153cb2c6c7b55eb
agwata9 <agwata9@gmail.com>
2023-06-14 09:35:47 +0000

    correction of some notable errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
 	process.stdout.write("Your name is: ${data}"):
 });
 
 process.stdin.on('end', () => {
-	process.stdin.on( 'Thank you for using our services');
+	process.stdin.on( 'This important software is now closing');
 });
