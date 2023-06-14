97c2b2d3f3024de335dc6052c7d5b7f17416a4ec
agwata9 <agwata9@gmail.com>
2023-06-14 09:29:12 +0000

    correction of some notable errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,9 @@
-process.stdin.setEncoding('utf8');
-
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (input) => {
-  const name = input.trim();
-  
-  if (name) {
-    console.log(`Your name is: ${name}`);
-  } else {
-    console.log('Your name is empty.');
-  }
+process.stdin.on('data', (data) => {
+	process.stdout.write("Your name is: ${data}"):
 });
 
 process.stdin.on('end', () => {
-  console.log('\nThis important software is now closing');
+	process.stdin.on( 'Thank you for using our services');
 });
