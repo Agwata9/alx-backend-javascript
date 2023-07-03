3ba1806efeebc683242dae014fa70685b20b6149
agwata9 <agwata9@gmail.com>
2023-07-03 05:42:03 +0000

    minor changes

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,30 +1,33 @@
 const fs = require('fs');
 
-function countStudents(path) {
+const countStudents = (path) => {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.trim().split('\n');
-    
-    if (lines.length === 0) {
-      throw new Error('The database is empty');
-    }
-    
-    const header = lines[0].split(',');
-    const students = lines.slice(1);
-    const studentCount = students.length;
-    
-    console.log(`Number of students: ${studentCount}`);
-    
-    header.forEach((field, index) => {
-      const fieldStudents = students
-        .map((student) => student.split(',')[index])
-        .filter((value) => value !== '');
-      
-      console.log(`Number of students in ${field}: ${fieldStudents.length}. List: ${fieldStudents.join(', ')}`);
-    });
-  } catch (error) {
-    console.error(`Cannot load the database: ${error.message}`);
-  }
-}
 
-countStudents('database.csv');
+    let lines = data.split(/\r?\n/);
+    lines.shift();
+    lines = lines.filter((line) => line !== '');
+
+    console.log(`Number of students: ${lines.length}`);
+
+    const cs = lines
+      .filter((line) => line.endsWith('CS'))
+      .map((line) => {
+        const student = line.split(',');
+        return student[0];
+      });
+    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+
+    const swe = lines
+      .filter((line) => line.endsWith('SWE'))
+      .map((line) => {
+        const student = line.split(',');
+        return student[0];
+      });
+    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+  } catch (err) {
+    throw Error('Cannot load the database');
+  }
+};
+
+module.exports = countStudents;
--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,31 +1,32 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const databaseFile = process.argv[2];
+const port = 1245;
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
+  const { method, url } = req;
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
-  if (req.url === '/') {
-    res.end('Hello Holberton School!\n');
-  } else if (req.url === '/students') {
-    countStudents(databaseFile)
-      .then(() => {
-        res.end('This is the list of our students\n');
-      })
-      .catch((error) => {
-        res.end(error.message);
-      });
-  } else {
-    res.statusCode = 404;
-    res.end('Not found\n');
+  if (method === 'GET') {
+    if (url === '/') {
+      res.end('Hello Holberton School!');
+    } else if (url === '/students') {
+      await countStudents(process.argv[2])
+        .then((value) => {
+          res.write('This is the list of our students\n');
+          res.write(`Number of students: ${value.lines.length}\n`);
+          res.write(`Number of students in CS: ${value.cs.length}. List: ${value.cs.join(', ')}\n`);
+          res.write(`Number of students in SWE: ${value.swe.length}. List: ${value.swe.join(', ')}`);
+          res.end();
+        })
+        .catch((err) => {
+          res.write('This is the list of our students\n');
+          res.end(err.message);
+        });
+    }
   }
 });
 
-app.listen(1245, () => {
-  console.log('Server listening on port 1245');
-});
+app.listen(port);
 
 module.exports = app;
-
